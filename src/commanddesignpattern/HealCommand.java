42059f73d949b544ca1ebff2b91295c1297045c0
Vishal Kosaraju <vishalkosaraju@Vishals-MacBook-Pro.local>
2021-02-26 18:24:43 -0500

    added HealCommand class

---
+++ src/commanddesignpattern/HealCommand.java
@@ -0,0 +1,14 @@
+package commanddesignpattern;
+public class HealCommand implements Command{
+	private Robot robot;
+	
+	public HealCommand(Robot robot)
+	{
+		this.heal = heal;
+	}
+	public void execute()
+	{
+		//robot();
+	}
+
+}
