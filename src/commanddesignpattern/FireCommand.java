0baee4c37532ec949b6be20c1fe5db8890831f26
Vishal Kosaraju <vishalkosaraju@Vishals-MacBook-Pro.local>
2021-02-26 18:22:35 -0500

    added FireCommand class

---
+++ src/commanddesignpattern/FireCommand.java
@@ -0,0 +1,14 @@
+package commanddesignpattern;
+public class FireCommand implements Command{
+	private Robot robot;
+	
+	public FireCommand(Robot robot)
+	{
+		this.fire = fire;
+	}
+	public void execute()
+	{
+		//robot();
+	}
+
+}
