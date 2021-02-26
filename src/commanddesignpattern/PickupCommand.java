dc6cff8fe14c9d3da004e9e98a350b2ab18eb6d4
Vishal Kosaraju <vishalkosaraju@Vishals-MacBook-Pro.local>
2021-02-26 18:15:42 -0500

    added PickupCommand class

---
+++ src/commanddesignpattern/PickupCommand.java
@@ -0,0 +1,14 @@
+
+public class PickupCommand implements Command{
+	private Robot robot;
+	
+	public PickupCommand(Robot robot)
+	{
+		this.pickup = pickup;
+	}
+	public void execute()
+	{
+		//robot();
+	}
+
+}
