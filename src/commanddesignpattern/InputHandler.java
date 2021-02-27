bcd72105620dbfc15a2e3947322840fe64a22922
Vishal Kosaraju <vishalkosaraju@Vishals-MacBook-Pro.local>
2021-02-26 23:40:34 -0500

    added changes to command class

--- src/commanddesignpattern/InputHandler.java
+++ src/commanddesignpattern/InputHandler.java
@@ -1,18 +1,22 @@
 package commanddesignpattern;
 
 import java.util.HashMap;
+
 /**
  * input handling class that uses a hashmap to select command
+ * 
  * @author miller
  */
 public class InputHandler {
 	public HashMap<String, Command> commands = new HashMap<>();
+
 	/**
 	 * constructor that creates and fills the hashmap with commands
+	 * 
 	 * @param robot object
 	 */
 	public InputHandler(Robot robot) {
-		PickUpCommand pc = new PickUpCommand(robot);
+		PickupCommand pc = new PickUpCommand(robot);
 		HealCommand hc = new HealCommand(robot);
 		JumpCommand jc = new JumpCommand(robot);
 		FireCommand fc = new FireCommand(robot);
@@ -21,23 +25,20 @@ public class InputHandler {
 		commands.put("fire", fc);
 		commands.put("heal", hc);
 	}
+
 	/**
 	 * method that accepts the input and chooses what to do
+	 * 
 	 * @param data entered
 	 */
 	public void inputEntered(String data) {
-		if (commands.containsKey(data))
-		{
+		if (commands.containsKey(data)) {
 			Command temp = commands.get(data);
 			temp.execute();
-		}
-		else if (data.equalsIgnoreCase("quit"))
-		{
+		} else if (data.equalsIgnoreCase("quit")) {
 			System.out.println("Goodbye");
 			System.exit(0);
-		}
-		else
-		{
+		} else {
 			System.out.println("Error");
 			System.exit(0);
 		}
