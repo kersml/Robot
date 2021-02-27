91ef540b86fd6fa38ecd625c03ea62c5b72161a9
kersml <77562509+kersml@users.noreply.github.com>
2021-02-26 19:00:26 -0500

    Update InputHandler.java

--- src/commanddesignpattern/InputHandler.java
+++ src/commanddesignpattern/InputHandler.java
@@ -1,13 +1,45 @@
 package commanddesignpattern;
 
+import java.util.HashMap;
+/**
+ * input handling class that uses a hashmap to select command
+ * @author miller
+ */
 public class InputHandler {
-
+	public HashMap<String, Command> commands = new HashMap<>();
+	/**
+	 * constructor that creates and fills the hashmap with commands
+	 * @param robot object
+	 */
 	public InputHandler(Robot robot) {
-		// TODO Auto-generated constructor stub
+		PickUpCommand pc = new PickUpCommand(robot);
+		HealCommand hc = new HealCommand(robot);
+		JumpCommand jc = new JumpCommand(robot);
+		FireCommand fc = new FireCommand(robot);
+		commands.put("pickup", pc);
+		commands.put("jump", jc);
+		commands.put("fire", fc);
+		commands.put("heal", hc);
 	}
-	
+	/**
+	 * method that accepts the input and chooses what to do
+	 * @param data entered
+	 */
 	public void inputEntered(String data) {
-		
+		if (commands.containsKey(data))
+		{
+			Command temp = commands.get(data);
+			temp.execute();
+		}
+		else if (data.equalsIgnoreCase("quit"))
+		{
+			System.out.println("Goodbye");
+			System.exit(0);
+		}
+		else
+		{
+			System.out.println("Error");
+			System.exit(0);
+		}
 	}
-
 }
